38ca2395c9da597d5c7c4154d120ff2c797e5d73
Matt McGee <mlmcgeenc@gmail.com>
2022-05-11 12:50:25 -0400

    add ability to edit task titles

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,9 @@ var evaluateCurrentTime = function (hour) {
 	if (moment(currentTime, "h a").isSame(timeOfEvent)) {
 		$(hour).parent().children(".description").addClass("present");
 	} else if (moment(currentTime, "h a").isBefore(timeOfEvent)) {
-        $(hour).parent().children(".description").addClass("future");
+		$(hour).parent().children(".description").addClass("future");
 	} else {
-        $(hour).parent().children(".description").addClass("past");
+		$(hour).parent().children(".description").addClass("past");
 	}
 };
 
@@ -23,5 +23,20 @@ setInterval(function () {
 // }, 1000 * 60 * 60);
 
 // clicking on a time block allows the user to enter an event title
+$(".container .description").on("click", function (event) {
+	console.log($(this).parent().attr("id"));
+	var text = $(this).children("p").text().trim();
+	var textInput = $("<textarea>").addClass("formControl").val(text).select();
+	$(this).children("p").replaceWith(textInput);
+	textInput.trigger("focus");
+});
+
+$(".container .description").on("blur", "textarea", function () {
+    console.log(this)
+	var text = $(this).val().trim();
+	var taskP = $("<p>").text(text);
+    $(this).removeClass("formControl");
+	$(this).replaceWith(taskP);
+});
 
 // save event title into local storage
